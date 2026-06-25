44e5502d19f65733a7644625ecc52e31fae1e9a5
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-25 17:18:25 -0300

    Update indexCadLivro.js

--- Assets/Js/indexCadLivro.js
+++ Assets/Js/indexCadLivro.js
@@ -6,11 +6,16 @@ import { initTema } from './comandosGlobais.js';
 initTema();
 
 const btnCadastro = document.querySelector('.btnCadLivro');
+const txtId = document.querySelector('.txtId');
+
+const ultimoID = Math.max(...livros.map(livro => livro.id)) + 1;
+txtId.value = ultimoID;
+txtId.disabled = true;
+
 btnCadastro.addEventListener('click', cadastrar);
-const txtId = document.querySelector('.txtId')
 
 function cadastrar() {
-    console.log('teste')
+    console.log('teste');
 }
 
 
