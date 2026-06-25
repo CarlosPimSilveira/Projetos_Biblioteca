c7c47425ab991b48aedbbf515a7cc5762b8ce24b
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-25 11:41:48 -0300

    estudos

--- Assets/Js/controleLivros.js
+++ Assets/Js/controleLivros.js
@@ -4,6 +4,69 @@ export const livros = [
         titulo: "O Hobbit",
         autor: "J. R. R. Tolkien",
         emprestado: 1,
-        qtd: 1
+        qtd: 3
+    },
+    {
+        id: 2,
+        titulo: "Dom Casmurro",
+        autor: "Machado de Assis",
+        emprestado: 0,
+        qtd: 5
+    },
+    {
+        id: 3,
+        titulo: "1984",
+        autor: "George Orwell",
+        emprestado: 1,
+        qtd: 2
+    },
+    {
+        id: 4,
+        titulo: "O Pequeno Príncipe",
+        autor: "Antoine de Saint-Exupéry",
+        emprestado: 0,
+        qtd: 4
+    },
+    {
+        id: 5,
+        titulo: "Harry Potter e a Pedra Filosofal",
+        autor: "J. K. Rowling",
+        emprestado: 1,
+        qtd: 6
+    },
+    {
+        id: 6,
+        titulo: "A Revolução dos Bichos",
+        autor: "George Orwell",
+        emprestado: 0,
+        qtd: 2
+    },
+    {
+        id: 7,
+        titulo: "Percy Jackson e o Ladrão de Raios",
+        autor: "Rick Riordan",
+        emprestado: 1,
+        qtd: 5
+    },
+    {
+        id: 8,
+        titulo: "O Senhor dos Anéis",
+        autor: "J. R. R. Tolkien",
+        emprestado: 0,
+        qtd: 3
+    },
+    {
+        id: 9,
+        titulo: "Capitães da Areia",
+        autor: "Jorge Amado",
+        emprestado: 1,
+        qtd: 4
+    },
+    {
+        id: 10,
+        titulo: "Memórias Póstumas de Brás Cubas",
+        autor: "Machado de Assis",
+        emprestado: 0,
+        qtd: 2
     }
 ];
