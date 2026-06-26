b04e8d00fd0c87a37c47d5cfc82de9b8a8e64f67
Carlos Pim <carlospimsilveira@gmail.com>
2026-06-26 11:00:27 -0300

    atualizações do projeto

--- Assets/Js/indexCadUsuario.js
+++ Assets/Js/indexCadUsuario.js
@@ -1,3 +1,13 @@
 // Conecta para uso de mudança de tema
 import { initTema } from './comandosGlobais.js';
 initTema();
+
+//Aplica mascara para o telefone!
+const tel = document.querySelector('.txtTelUsuario');
+tel.addEventListener('input', (e) => {
+    let value = e.target.value;
+    value = value.replace(/\D/g, ''); // remove tudo que não for número
+    value = value.replace(/(\d{2})(\d)/, '($1) $2');
+    value = value.replace(/(\d{5})(\d)/, '$1-$2');
+    e.target.value = value;
+});
